07e11afc9dbb469d7fe729992ec33272f044316b
Anastasiia <raznas63@gmail.com>
2020-08-29 18:42:23 +0400

    fist commit

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from "../components/Header/Header";
+import Profile from "./Profiles";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
